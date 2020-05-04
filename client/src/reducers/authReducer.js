2dfc8b41dcd83d31e1d02a151d736c2f9c5d0108
SHIHCHANG <axray@hotmail.com>
2020-05-04 23:11:04 +0800

    doing payment

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../actions/types';
 
 export default function(state = null, action) {
-    console.log(action);
+    // console.log(action);
     switch(action.type) {
         case actionTypes.FETCH_USER:
             return action.payload || false;
