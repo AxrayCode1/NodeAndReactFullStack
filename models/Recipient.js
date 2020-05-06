d696ad858669401d8df2d99643c342ce68688e71
SHIHCHANG <axray@hotmail.com>
2020-05-06 16:58:09 +0800

    doing surveys system...

---
+++ models/Recipient.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const recipientSchema = new Schema({
+    email: String,
+    respond: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = recipientSchema;
